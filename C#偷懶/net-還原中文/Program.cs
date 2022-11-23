76d7341712399502d3a53647720e751ea6f52a6a
07050862backup <110028879+07050862backup@users.noreply.github.com>
2022-11-23 18:42:39 +0800

    Add files via upload

---
+++ C#偷懶/net-還原中文/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 將單字加上引號並去除空白
+{
+    static class Program {
+        /// <summary>
+        /// 應用程式的主要進入點。
+        /// </summary>
+        [STAThread]
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
