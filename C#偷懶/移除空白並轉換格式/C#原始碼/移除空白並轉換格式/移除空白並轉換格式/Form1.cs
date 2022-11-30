de301cd8ce1fe15ae311a33427c7c7dab9a2db49
07050862backup <110028879+07050862backup@users.noreply.github.com>
2022-11-30 19:04:10 +0800

    Add files via upload

---
+++ C#偷懶/移除空白並轉換格式/C#原始碼/移除空白並轉換格式/移除空白並轉換格式/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace 移除空白並轉換格式 {
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            text_output.Focus();
+            text_output.Text = "";
+            String input = text_input.Text;
+
+           
+            string[] str = new string[text_input.Lines.Length];
+
+            for (int i = 0; i < text_input.Lines.Length; i++)
+            {
+                str[i] = text_input.Lines[i];
+                if (String.IsNullOrEmpty(str[i])  || String.IsNullOrWhiteSpace(str[i]) )
+                    continue;
+                else
+                {
+                    text_output.Text += "\"" + str[i] + "\"" + "," + "\n";  //有逗號
+                    //text_output.Text += "\"" + str[i] + "\"" + "\n";      //無逗號
+                    text_output.Text = text_output.Text.Replace(" ", "　");
+                    text_output.Text = text_output.Text.Replace("　", "");
+                    text_output.Text = text_output.Text.Replace("\t", "");
+                }
+            }
+        }
+
+
+
+    }
+}
