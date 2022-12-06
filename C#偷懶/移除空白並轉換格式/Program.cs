5024bd8878899fcdb4cb3d6973a8f20ebc59176d
07050862backup <110028879+07050862backup@users.noreply.github.com>
2022-12-06 21:27:49 +0800

    Add files via upload

---
+++ C#偷懶/移除空白並轉換格式/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 移除空白並轉換格式 {
+    static class Program {
+        /// <summary>
+        /// 應用程式的主要進入點。
+        /// </summary>
+        static void Main() {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
