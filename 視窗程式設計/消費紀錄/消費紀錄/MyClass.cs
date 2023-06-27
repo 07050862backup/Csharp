131ce82f13d7020f1e2ae292ae4f4d5d56347b3e
07050862backup <110028879+07050862backup@users.noreply.github.com>
2023-06-27 08:30:47 +0800

    Add files via upload

---
+++ 視窗程式設計/消費紀錄/消費紀錄/MyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace 消費紀錄
+{
+    public struct ITEM
+    {
+        public string name;
+        public float unitPrice;
+        public uint number;
+
+        public ITEM(string name, float unitPrice, uint number)
+        {
+            this.name = name;
+            this.unitPrice = unitPrice;
+            this.number = number;
+        }
+    }
+}
