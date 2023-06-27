2c0e91718052dc0ebd34247bd52f6463d1d9ee00
07050862backup <110028879+07050862backup@users.noreply.github.com>
2023-06-27 08:26:46 +0800

    Add files via upload

---
+++ 視窗程式設計/讀入圖檔/讀入圖檔/Form1.Designer.cs
@@ -0,0 +1,253 @@
+﻿
+namespace 讀入圖檔
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.LoadButton = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.HScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 52);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(12, 12);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(103, 29);
+            this.LoadButton.TabIndex = 1;
+            this.LoadButton.Text = "Load Image";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(806, 247);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(61, 27);
+            this.numericUpDown1.TabIndex = 2;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.NumValueChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(873, 171);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(109, 27);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ChannelSelectChanged);
+            // 
+            // HScrollBar1
+            // 
+            this.HScrollBar1.Location = new System.Drawing.Point(589, 75);
+            this.HScrollBar1.Name = "HScrollBar1";
+            this.HScrollBar1.Size = new System.Drawing.Size(356, 26);
+            this.HScrollBar1.TabIndex = 4;
+            this.HScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.ZoomHScroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(526, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 19);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Zoom";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(526, 174);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 19);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Color";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(801, 174);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(66, 19);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Channel";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(526, 249);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 19);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Thresholding";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(646, 249);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(149, 19);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Lower Thresholding";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(646, 307);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(149, 19);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Upper Thresholding";
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(589, 172);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(97, 23);
+            this.radioButton1.TabIndex = 13;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Gray scall";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(708, 172);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(87, 23);
+            this.radioButton2.TabIndex = 14;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "RGB888";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(806, 305);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(61, 27);
+            this.numericUpDown2.TabIndex = 15;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(542, 305);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(77, 23);
+            this.checkBox1.TabIndex = 16;
+            this.checkBox1.Text = "Enable";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(905, 247);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(86, 23);
+            this.checkBox2.TabIndex = 17;
+            this.checkBox2.Text = "Reverse";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1006, 551);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.HScrollBar1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button LoadButton;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.HScrollBar HScrollBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ 視窗程式設計/讀入圖檔/讀入圖檔/Form1.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 讀入圖檔
+{
+    public partial class Form1 : Form
+    {
+        private int imageH;
+        private int imageW;
+        private Image rgbImage;
+        private Image rImage;
+        private Image gImage;
+        private Image bImage;
+        private Image grayImage;
+        private Image thresholdingImage;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                rgbImage = Image.FromFile(ofd.FileName);
+                rImage = (Image)GetRChannel(new Bitmap(rgbImage));
+                gImage = (Image)GetGChannel(new Bitmap(rgbImage));
+                bImage = (Image)GetBChannel(new Bitmap(rgbImage));
+                RGBtoGray(rgbImage,ref grayImage);
+                thresholdingImage = Image.FromFile(ofd.FileName);
+                if (rgbImage.Width > rgbImage.Height)
+                    pictureBox1.Height = 400 * rgbImage.Height / rgbImage.Width;
+                else
+                    pictureBox1.Width = 400 * rgbImage.Width / rgbImage.Height;
+                imageH = pictureBox1.Height;
+                imageW = pictureBox1.Width;
+                pictureBox1.Image = rgbImage;
+                HScrollBar1.Value = 100;
+                radioButton2.Checked = true;//RGB888
+
+                comboBox1.SelectedIndex = 0;
+                numericUpDown1.Enabled = false;
+                numericUpDown2.Enabled = false;
+                checkBox1.Enabled = false;
+                checkBox2.Enabled = false;
+            }
+        }
+
+        private Image GetRChannel(Bitmap bm)
+        {
+            Bitmap newBitmap = new Bitmap(bm.Width, bm.Height);
+            for(int y = 0; y < bm.Height ; y++)
+            {
+                for (int x = 0; x < bm.Width; x++)
+                {
+                    Color pixelColor = bm.GetPixel(x, y);
+                    newBitmap.SetPixel(x, y, Color.FromArgb(pixelColor.R, 0, 0));
+                }
+            }
+            return newBitmap;
+        }
+        private Image GetGChannel(Bitmap bm)
+        {
+            Bitmap newBitmap = new Bitmap(bm.Width, bm.Height);
+            for (int y = 0; y < bm.Height; y++)
+            {
+                for (int x = 0; x < bm.Width; x++)
+                {
+                    Color pixelColor = bm.GetPixel(x, y);
+                    newBitmap.SetPixel(x, y, Color.FromArgb(0, pixelColor.G, 0));
+                }
+            }
+            return newBitmap;
+        }
+        private Image GetBChannel(Bitmap bm)
+        {
+            Bitmap newBitmap = new Bitmap(bm.Width, bm.Height);
+            for (int y = 0; y < bm.Height; y++)
+            {
+                for (int x = 0; x < bm.Width; x++)
+                {
+                    Color pixelColor = bm.GetPixel(x, y);
+                    newBitmap.SetPixel(x, y, Color.FromArgb(0, 0, pixelColor.B));
+                }
+            }
+            return newBitmap;
+        }
+
+        private void RGBtoGray(Image source, ref Image dest)
+        {
+            Bitmap sourceBitmap = (Bitmap)source;
+            dest = (Image)source.Clone();
+            Bitmap destBitmap = (Bitmap)dest;
+            for(int y = 0;y<sourceBitmap.Height;y++)
+            {
+                for (int x = 0; x < sourceBitmap.Width; x++)
+                {
+                    Color pixelColor = sourceBitmap.GetPixel(x, y);
+                    int pixelLuminance = (int)(pixelColor.R * 0.2126 + pixelColor.G * 0.7152 + pixelColor.B * 0.0722);
+                    destBitmap.SetPixel(x, y, Color.FromArgb(pixelLuminance, pixelLuminance, pixelLuminance));
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            HScrollBar1.Maximum = 109;
+            HScrollBar1.Minimum = 1;
+            comboBox1.Items.Add("RGB Channel");
+            comboBox1.Items.Add("R Channel");
+            comboBox1.Items.Add("G Channel");
+            comboBox1.Items.Add("B Channel");
+            numericUpDown1.Maximum = 255;
+            numericUpDown1.Minimum = -1;
+            numericUpDown2.Maximum = 255;
+            numericUpDown2.Minimum = -1;
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+        }
+
+      
+
+        private void ZoomHScroll(object sender, ScrollEventArgs e)
+        {
+            pictureBox1.Height = (int)(imageH / 100.0 * HScrollBar1.Value);
+            pictureBox1.Width = (int)(imageW / 100.0 * HScrollBar1.Value);
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(radioButton1.Checked)
+            {
+                pictureBox1.Image = grayImage;
+                checkBox1.Enabled = true;
+                checkBox2.Enabled = true;
+                numericUpDown1.Enabled = false;
+                numericUpDown2.Enabled = false;
+                numericUpDown1.Value = 100;
+                numericUpDown2.Value = -1;
+                checkBox1.Checked = false;
+            }
+            else if(radioButton2.Checked)
+            {
+                pictureBox1.Image = rgbImage;
+                comboBox1.SelectedIndex = 0;
+                numericUpDown1.Enabled = false;
+                numericUpDown2.Enabled = false;
+                checkBox1.Enabled = false;
+                checkBox2.Enabled = false;
+            }
+        }
+
+        private void ChannelSelectChanged(object sender, EventArgs e)
+        {
+            switch(comboBox1.SelectedIndex)
+            {
+                case 0:
+                    pictureBox1.Image = rgbImage;
+                    break;
+                case 1:
+                    pictureBox1.Image = rImage;
+                    break;
+                case 2:
+                    pictureBox1.Image = gImage;
+                    break;
+                case 3:
+                    pictureBox1.Image = bImage;
+                    break;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked == true)
+            {
+                GetThresholdingImage(rgbImage, ref thresholdingImage,
+                    (int)numericUpDown1.Value, (int)numericUpDown2.Value, checkBox2.Checked);
+                pictureBox1.Image = thresholdingImage;
+                numericUpDown1.Enabled = true;
+                numericUpDown2.Enabled = true;
+            }
+            else
+            {
+                pictureBox1.Image = grayImage;
+                numericUpDown1.Enabled = false;
+                numericUpDown2.Enabled = false;
+
+            }
+        }
+
+        private void NumValueChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked == true)
+            {
+                GetThresholdingImage(rgbImage, ref thresholdingImage,
+                    (int)numericUpDown1.Value, (int)numericUpDown2.Value, checkBox2.Checked);
+                pictureBox1.Image = thresholdingImage;
+            }
+        }
+
+        private void GetThresholdingImage(Image source, ref Image dest, int Lower, int Upper, bool reverse)
+        {
+            Bitmap sourceBitmap = (Bitmap)source;
+            dest.Dispose();
+            dest = (Image)source.Clone();
+            Bitmap destBitmap = (Bitmap)dest;
+            for(int y = 0;y < sourceBitmap.Height ;y++ )
+            {
+                for(int x = 0;x<sourceBitmap.Width ;x++ )
+                {
+                    Color pixelColor = sourceBitmap.GetPixel(x, y);
+                    if(Upper >= 0 && Upper > Lower)
+                    {
+                        if (pixelColor.R > Lower && pixelColor.R < Upper)
+                            if (reverse == true)
+                                destBitmap.SetPixel(x, y, Color.FromArgb(0, 0, 0));
+                            else
+                                destBitmap.SetPixel(x, y, Color.FromArgb(0, 0, 0));
+                        else
+                            if (reverse == true)
+                                destBitmap.SetPixel(x, y, Color.FromArgb(255, 255, 255));
+                            else
+                                destBitmap.SetPixel(x, y, Color.FromArgb(0, 0, 0));
+                    }
+                    else
+                    {
+                        if(pixelColor.R > Lower)
+                            if(reverse == true)
+                                destBitmap.SetPixel(x, y, Color.FromArgb(0, 0, 0));
+                            else
+                                destBitmap.SetPixel(x, y, Color.FromArgb(255, 255, 255));
+                        else
+                            if (reverse == true)
+                                destBitmap.SetPixel(x, y, Color.FromArgb(255, 255, 255));
+                            else
+                                destBitmap.SetPixel(x, y, Color.FromArgb(0, 0, 0));
+                    }
+                }
+            }
+        }
+    }
+}
